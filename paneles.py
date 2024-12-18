ebd77f86971054c618c731bd3e7f4d7734133ee7
CristobalTortugauwu <cristobal.suazo@ug.uchile.cl>
2024-12-18 14:37:51 -0300

    added a main function

--- paneles.py
+++ paneles.py
@@ -18,9 +18,10 @@ def max_paneles(x,y,a,b):
             j+=b
             contador+=1
         i+=a
-    return contador       
-print(max_paneles(2,4,1,2))
-print(max_paneles(3,5,1,2))
-print(max_paneles(1,10,2,2))
-print(max_paneles(2,15,0,0))
-print(max_paneles(2,0,1,1))
+    return contador    
+if __name__ == '__main__':   
+    print(max_paneles(2,4,1,2))
+    print(max_paneles(3,5,1,2))
+    print(max_paneles(1,10,2,2))
+    print(max_paneles(2,15,0,0))
+    print(max_paneles(2,0,1,1))
