c84933af828b0cc9c14f32cd3061b51f341b08ec
CristobalTortugauwu <cristobal.suazo@ug.uchile.cl>
2024-12-18 14:36:32 -0300

    solucion parcial, se cubren algunos casos bordes

--- paneles.py
+++ paneles.py
@@ -2,14 +2,15 @@
 def max_paneles(x,y,a,b):
     i=0
     contador = 0
+    if a==0 or b==0 or x==0 or y==0:
+        print("dimensiones invalidas")
+        return 0
     while x-i>=b:
         j=0
         while y-j>=a:
             j+=a 
             contador +=1
         i+=b
-    ##preguntar si puedo colocarlos acostados
-    print(j,i)
     new_j=0
     while x-i>=a:
         j=new_j
@@ -21,3 +22,5 @@ def max_paneles(x,y,a,b):
 print(max_paneles(2,4,1,2))
 print(max_paneles(3,5,1,2))
 print(max_paneles(1,10,2,2))
+print(max_paneles(2,15,0,0))
+print(max_paneles(2,0,1,1))
